07e206dfed1245f20fc7242e430d8f917d0522bb
Grimm1859 <nguyentrungkien12092@gmail.com>
2026-09-04 16:36:58 +0700

    Finish 08_removeFrom_Array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const item of args){
+        for (let i = 0 ; i < arr.length ; i ++){
+            if (arr.indexOf(item) == -1){
+                continue;
+            } else {
+                arr.splice(arr.indexOf(item),1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
