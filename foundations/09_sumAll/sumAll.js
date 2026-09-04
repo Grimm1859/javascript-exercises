0a1aba48b38a8f6593f02901dbe84935aad5c71d
Grimm1859 <nguyentrungkien12092@gmail.com>
2026-09-04 17:00:19 +0700

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+function sumAll(no1,no2){
+    if(!isValid(no1) || !isValid(no2)){
+        return 'ERROR'
+    } else {
+    let arr = [no1,no2].sort((a,b) => a - b)
+    let sum = 0;
+    for (let i = arr[0];  i <= arr[1] ; i++){
+        sum = sum + i
+    }
+    return sum;
+}
 };
 
+function isValid(num){
+    return Number.isInteger(num) && num > 0;
+}
 // Do not edit below this line
 module.exports = sumAll;
