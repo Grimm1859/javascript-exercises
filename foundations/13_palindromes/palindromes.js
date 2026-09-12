463efb1356420ca70184354f891de7757ec8722e
Grimm1859 <nguyentrungkien12092@gmail.com>
2026-09-12 09:37:01 +0700

    Finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const reverseString = function(word){
+	let arr = word.split('');
+	arr = arr.reverse();
+	return arr.join('');
+}	
 
+const convertWord = function(word){
+	const lowercase = word.toLowerCase();
+	const noSpace = lowercase.replaceAll(" ","");
+	return noSpace.replace(/[^\w\s]|_/g, "");
+}
+
+const palindromes = function (word){
+	if(convertWord(word) == convertWord(reverseString(word)))
+	{
+		return true;
+	} else {
+		return false;
+	}
+	
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
