258708d6d93f2088cb082a5699fc85ea2f97e52e
Grimm1859 <nguyentrungkien12092@gmail.com>
2026-09-12 10:44:38 +0700

    Finish findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function (birth, death){
+	if(!death){
+		death = new Date().getFullYear();
+	}
+	return death - birth;
+}
 
+const findTheOldest = function(people) {
+	const peopleMapped = people.map(person => (
+		{
+			name: person.name,
+			age: getAge(person.yearOfBirth,person.yearOfDeath),
+		}));
+	const peopleSorted = peopleMapped.sort((a, b) => b.age - a.age)
+	console.log(peopleSorted);
+	return peopleSorted[0];
 };
 
 // Do not edit below this line
