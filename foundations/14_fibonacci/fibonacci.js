44300fb66e79198439cea8b434934a6dc32bf4cd
Grimm1859 <nguyentrungkien12092@gmail.com>
2026-09-12 10:09:48 +0700

    Finish fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+	let arr = [0,1];
+	for (let i = 0; i < 30; i++) {
+		arr.push(arr[arr.length-1] + arr[arr.length-2])
+	}
+	if (num < 0) {
+		return "OOPS";
+	} else {
+		return arr[num];
+	}
 };
 
 // Do not edit below this line
